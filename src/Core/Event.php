52b0895f3b907e47d394eaf335ea352f99afadcf
zhanghw <zhanghw@infobird.com>
2019-01-18 15:36:12 +0800

    增加广播的分组id

--- src/Core/Event.php
+++ src/Core/Event.php
@@ -126,6 +126,7 @@ class Event{
         $this->debug('broadcastself', 'broadcast connections');
         $this->debug('broadcastself - data', $data);
         $this->debug('broadcastself - node_fds', $this->app_server->nodeFDs);
+	    $data->plume_group_id = $groupID;
         foreach ($connections as $fd) {
             if(isset($this->app_server->nodeFDs[$fd])){
                 $this->debug('broadcastself', 'nodeClient fd is '.$fd);
